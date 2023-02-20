188767ee619a43e1efe8080716c7dee4a8eb2cf4
KumarYeoley214 <110538676+KumarYeoley214@users.noreply.github.com>
2023-02-21 00:34:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public/'))
-app.listen(3000), function(){
-    console.log('Server listening at 3000');
+app.listen(9000), function(){
+    console.log('Server listening at 9000');
 };
